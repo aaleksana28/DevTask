372f9e28b90562ea67ac16a2ebde9242b173a1ab
aaleksana28 <aaleksana28@gmail.com>
2024-03-15 20:54:34 +0700

    Поправила получение данных с ЦБ РФ

--- src/Parser.php
+++ src/Parser.php
@@ -5,13 +5,9 @@ namespace Aleks\Devtask;
 class Parser {
     public function getDataFromCB() {
         $data = file_get_contents("http://www.cbr.ru/scripts/XML_daily.asp");
-        $parser = xml_parser_create();
-        xml_parser_set_option($parser, XML_OPTION_TARGET_ENCODING, "UTF-8");
-        xml_parser_set_option($parser, XML_OPTION_CASE_FOLDING, 0);
-        xml_parser_set_option($parser, XML_OPTION_SKIP_WHITE, 1);
-        xml_parse_into_struct($parser, trim($data), $xml_values);
-        xml_parser_free($parser);
-        return $xml_values;
+        $xml_data = simplexml_load_string($data);
+
+        return $xml_data;
     }
 
 }
