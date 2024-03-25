5267d63eb413d00d0c9f926ebb0b59c239cf182f
aaleksana28 <aaleksana28@gmail.com>
2024-03-25 20:30:27 +0700

    Сделала GUI интерфейс для конвертации валют

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
+<?php
+include './vendor/autoload.php';
+
+$valute_list = \Aleks\Devtask\Parser::getDataFromFile();
+
+if (isset($_GET['submit'])) {
+    $value = (float)$_GET['valute_value_src'] ?? 0;
+    $source_valute = $valute_list[$_GET['valute_value_type_src']];
+    $needle_valute = $valute_list[$_GET['valute_value_type_needle']];
+    $rub_value = (float)$source_valute['VALUTE_VALUE'] * $value ;
+    $needle_value = $rub_value / (float)$needle_valute['VALUTE_VALUE'];
+}
+
+?>
+
 <!doctype html>
 <html lang="en">
 <head>
@@ -5,19 +20,26 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Конвертация валют</title>
 </head>
 <body>
-
+    <form action="/" method="get">
+        <?php if (!isset($needle_valuee)) echo 'Результат перевода получился: ' . $needle_value . ' ' . $needle_valute['NAME'];?>
+        <label for="">Сумма перевода:</label>
+        <input type="number" name="valute_value_src">
+        <label for="">Исходная валюта:</label>
+        <select name="valute_value_type_src">
+            <?php foreach ($valute_list as $item):?>
+                <option value="<?php echo $item['SHORT_KEY'];?>"><?php echo $item['NAME']?></option>
+            <?php endforeach;?>
+        </select>
+        <label for="">Нужная валюта:</label>
+        <select name="valute_value_type_needle">
+            <?php foreach ($valute_list as $item):?>
+                <option value="<?php echo $item['SHORT_KEY'];?>"><?php echo $item['NAME']?></option>
+            <?php endforeach;?>
+        </select>
+        <input type="submit" name="submit" value="Конвертировать">
+    </form>
 </body>
 </html>
-
-<?php
-
-include './vendor/autoload.php';
-
-$parser = new \Aleks\Devtask\Parser();
-var_dump($parser->getDataFromCB());
-?>
-
-
--- src/Parser.php
+++ src/Parser.php
@@ -10,4 +10,25 @@ class Parser {
         return $xml_data;
     }
 
+    public static function getDataFromFile() {
+        $date = date('d_m_y');
+
+        if (!file_exists($_SERVER['DOCUMENT_ROOT'] . '/data/' . $date . '.csv')) {
+            shell_exec('php ' . $_SERVER['DOCUMENT_ROOT'] . '/cron/parser.php');
+        }
+        $f = fopen($_SERVER['DOCUMENT_ROOT'] . '/data/' . $date . '.csv', 'r');
+
+        $data = [];
+        while ($item = fgetcsv($f, null, ';')) {
+            $data[$item[1]] = [
+                "SHORT_KEY" => $item[1],
+                "RUB_COUNT" => $item[2],
+                "NAME" => $item[3],
+                "VALUTE_VALUE" => $item[5]
+            ];
+        }
+
+        return $data;
+    }
+
 }
